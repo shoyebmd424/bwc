b777dc28b3ea0af02fab8fe9c142a7f578a24640
Md <96253133+shoyebmd424@users.noreply.github.com>
2024-07-28 08:05:33 +0530

    vite config

--- bwc-client/vite.config.js
+++ bwc-client/vite.config.js
@@ -12,8 +12,15 @@
 // });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import alias from "@rollup/plugin-alias";
+import path from "path";
 
 export default defineConfig({
-  base: "/bwc/", // Replace 'bwc' with your repository name
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "/bwc/", // Replace 'bwc' with your repository name
 });
