f0ba92d5f9be23d0e4089769ae42ee4d900c296f
Md <96253133+shoyebmd424@users.noreply.github.com>
2024-07-28 08:12:56 +0530

    vite config

--- bwc-client/vite.config.js
+++ bwc-client/vite.config.js
@@ -12,7 +12,6 @@
 // });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import alias from "@rollup/plugin-alias";
 import path from "path";
 
 export default defineConfig({
